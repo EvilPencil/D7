9d820e2b6f3dc758c00c356172d109dd6883d8dc
Andrey Danilov <danilovandrey@hotmail.com>
2024-11-22 17:59:35 +0300

    CrockfordObject

---
+++ CrockfordObject.js
@@ -0,0 +1,13 @@
+// Douglas Crockford Object Creation
+
+function constructor(spec) {
+  let {member} = spec,
+  {other} = other_constructor(spec),
+  method = function () {
+    // member, other, method
+  };
+  return Object.freeze({
+    method,
+    other
+  });
+}
